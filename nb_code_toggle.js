4414b6bf18efdd3c981cd9b3d780a41886abcbf4
Paul Hayne <31781956+phayne@users.noreply.github.com>
2017-09-24 10:31:24 -0700

    Create nb_code_toggle.js

---
+++ nb_code_toggle.js
@@ -0,0 +1,10 @@
+code_show=true; 
+function code_toggle() {
+ if (code_show){
+ $('div.input').hide();
+ } else {
+ $('div.input').show();
+ }
+ code_show = !code_show
+} 
+$( document ).ready(code_toggle);
